2c7a3b3dac61d103406aab6f04bf1cabb6c60abc
Francisco <franc.bustoss@gmail.com>
2022-11-23 23:34:56 +0000

    delete consoles logs

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,8 +26,6 @@ const callback = ( req, res = response ) => {
         return;
     }
 
-    console.log( CLIENT_ID, CLIENT_SECRET, frontEndUri);
-
     axios({
         method: 'post',
         url: 'https://accounts.spotify.com/api/token',
@@ -49,7 +47,6 @@ const callback = ( req, res = response ) => {
         }
     })
     .catch ( ( error ) => {
-        console.log( error );
         res.send( error );
     });
 }
