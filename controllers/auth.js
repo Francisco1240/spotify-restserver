f3b8def1bc60f67943252cfdc7b1c31addd28d4a
Francisco <franc.bustoss@gmail.com>
2022-11-24 21:51:09 +0000

    Catch expanded to show errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,7 +26,7 @@ const callback = ( req, res = response ) => {
         return;
     }
 
-    console.log( 'client data ',CLIENT_ID, CLIENT_SECRET, frontEndUri )
+    console.log( CLIENT_ID, CLIENT_SECRET, frontEndUri )
 
     axios({
         method: 'post',
@@ -42,17 +42,30 @@ const callback = ( req, res = response ) => {
         }
     })
     .then( ( response ) => {
-        console.log('Response --->',response)
         if ( response.status === 200 ) {
             res.send(  response.data );
         } else {
             res.send( response );
         }
     })
-    .catch ( ( error ) => {
-        console.log(error)
-        res.send( error );
-    });
+    .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      });
 }
 
 module.exports = {
