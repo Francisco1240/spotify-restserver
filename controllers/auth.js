7166e1821f93de96991befb8a71358c04807b1dc
Francisco <franc.bustoss@gmail.com>
2022-11-24 23:09:07 +0000

    redirect uri changed in axios call

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,11 +8,6 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const redirectUri = process.env.REDIRECT_URI;
 const frontEndUri = process.env.FRONTEND_URI;
 
-// const CLIENT_ID = '0a6ab43cb7c5426ab9182069574572a9';
-// const CLIENT_SECRET = 'b9b65f58880e481aa35c339db3f456a4';
-// const redirectUri = 'http://localhost:4200/auth/callback';
-// const frontEndUri = 'http://localhost:3000';
-
 const stateKey = 'spotify_auth_state';
 
 const login = (req, res = response ) => {
@@ -31,33 +26,29 @@ const callback = ( req, res = response ) => {
         return;
     }
 
-    console.log( CLIENT_ID, CLIENT_SECRET, frontEndUri )
-
-    console.log( code )
-
-    // axios({
-    //     method: 'post',
-    //     url: 'https://accounts.spotify.com/api/token',
-    //     data : {
-    //         code: code,
-    //         redirect_uri: frontEndUri,
-    //         grant_type: 'authorization_code'
-    //     },
-    //     headers: {
-    //         'content-type': 'application/x-www-form-urlencoded',
-    //         'Authorization': 'Basic ' + Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET ).toString('base64')
-    //     }
-    // })
-    // .then( ( response ) => {
-    //     if ( response.status === 200 ) {
-    //         res.send(  response.data );
-    //     } else {
-    //         res.send( response );
-    //     }
-    // })
-    // .catch ( ( error ) => {
-    //     res.send( error );
-    // });
+    axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data : {
+            code: code,
+            redirect_uri: 'http://localhost:4200/auth/callback',
+            grant_type: 'authorization_code'
+        },
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET ).toString('base64')
+        }
+    })
+    .then( ( response ) => {
+        if ( response.status === 200 ) {
+            res.send(  response.data );
+        } else {
+            res.send( response );
+        }
+    })
+    .catch ( ( error ) => {
+        res.send( error );
+    });
 }
 
 module.exports = {
